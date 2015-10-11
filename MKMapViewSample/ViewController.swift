e4b1f14b1ea0f9f4a344403d295a134d3fc7d67d
koogawa <koogawa.app@gmail.com>
2015-10-11 10:01:20 +0900

    Add red pin

--- MKMapViewSample/ViewController.swift
+++ MKMapViewSample/ViewController.swift
@@ -3,16 +3,30 @@
 //  MKMapViewSample
 //
 //  Created by koogawa on 2015/10/11.
-//  Copyright © 2015年 koogawa. All rights reserved.
+//  Copyright © 2015 koogawa. All rights reserved.
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
+        let coordinate = CLLocationCoordinate2DMake(37.331652997806785, -122.03072304117417)
+        let span = MKCoordinateSpanMake(0.003, 0.003)
+        let region = MKCoordinateRegionMake(coordinate, span)
+        mapView.setRegion(region, animated:true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "title"
+        annotation.subtitle = "subtitle"
+        self.mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - MKMapView delegate
+
+    // Called when the location of the user was updated
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        print(__FUNCTION__)
+    }
 
 }
 
